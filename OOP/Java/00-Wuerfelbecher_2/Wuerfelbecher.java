ab9f44645e91ba0c3b954efe320f1e0f04437433
J. Neugebauer <ngb@helmholtz-bi.de>
2021-10-05 23:13:20 +0200

    Neue Versionen generiert

---
+++ OOP/Java/00-Wuerfelbecher_2/Wuerfelbecher.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+public class Wuerfelbecher {
+
+    private Wuerfel[] wuerfel;
+
+    public Wuerfelbecher() {
+        wuerfel = new Wuerfel[5];
+    }
+
+    public void neueWuerfel() {
+        Random r = new Random();
+        for( int x = 0; x < wuerfel.length; x += 1 ) {
+            Wuerfel w = new Wuerfel(r);
+            wuerfel[x] = w;
+        }
+    }
+
+    public void werfen() {
+        for( int y = 0; y < wuerfel.length; y += 1 ) {
+            wuerfel[y].werfen();
+        }
+    }
+
+    public int getWuerfelsumme() {
+        int summe = 0;
+        for( int i = 0; i < wuerfel.length; i += 1 ) {
+            summe += wuerfel[i].getZahl();
+        }
+        return summe;
+    }
+
+}
---
+++ OOP/Java/00-Wuerfelbecher_ML/Wuerfelbecher.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class Wuerfelbecher {
+
+    private Wuerfel[] wuerfel;
+
+    public Wuerfelbecher() {
+        wuerfel = new Wuerfel[5];
+    }
+
+    public void neueWuerfel() {
+        for( int x = 0; x < wuerfel.length; x += 1 ) {
+        }
+    }
+
+    public void werfen() {
+        for( int y = 0; y < wuerfel.length; y += 1 ) {
+            wuerfel[y].werfen();
+        }
+    }
+
+    public int getWuerfelsumme() {
+        int summe = 0;
+        for( int i = 0; i < wuerfel.length; i += 1 ) {
+            summe += wuerfel[i].getZahl();
+        }
+        return summe;
+    }
+
+}
